e30909b436315c55444db6e74c907eb5c36889f9
mterwilleger <93240105+mterwilleger@users.noreply.github.com>
2023-02-02 20:11:06 -0700

    Added Player and Enemy

--- Hero Born/Assets/Scripts/LearningCurve.cs
+++ Hero Born/Assets/Scripts/LearningCurve.cs
@@ -5,13 +5,29 @@ using UnityEngine;
 public class LearningCurve : MonoBehaviour
 {
 
-    public int currentAge = 30; 
+    private int currentAge = 30; 
     public int addedAge = 1;
 
+    public float ageGap = 1.75f;
+    public string lastName = "Terwilleger";
+    public bool isAge = true;
     // Start is called before the first frame update
     void Start()
     {
-        ComputeAge();
+        int characterLevel = 32;
+        int nextSkillLevel = GenerateCharacter("Sokka", characterLevel);
+        Debug.Log(nextSkillLevel);
+        Debug.Log(GenerateCharacter("Katara", characterLevel));
+        //ComputeAge();
+
+        //Debug.Log($"A string can have variables like {lastName} inserted directly!");
+
+        //Debug.Log(lastName * ageGap);
+    }
+    public int GenerateCharacter(string name, int level)
+    {
+        //Debug.LogFormat("Character: {0} - Level: {1}", name, level);
+        return level + 5;
     }
 
     void ComputeAge()
