c9e798b42f0992ef58af6cc200c2f09944e15714
mterwilleger <93240105+mterwilleger@users.noreply.github.com>
2023-02-09 21:21:00 -0700

    Added Jump

--- Hero Born/Assets/Scripts/PlayerBehavior.cs
+++ Hero Born/Assets/Scripts/PlayerBehavior.cs
@@ -6,15 +6,20 @@ public class PlayerBehavior : MonoBehaviour
 {
     public float moveSpeed = 10f;
     public float rotateSpeed = 75f;
+    public float jumpVelocity = 5f;
+    public float distanceToGround = 0.1f;
+    public LayerMask groundLayer;
 
     private float vInput;
     private float hInput;
-
     private Rigidbody _rb;
+
+    private CapsuleCollider _col;
 
     void Start ()
     {
         _rb = GetComponent<Rigidbody>();
+        _col = GetComponent<CapsuleCollider>();
     }
     // Update is called once per frame
     void Update()
@@ -29,9 +34,19 @@ public class PlayerBehavior : MonoBehaviour
     }
     void FixedUpdate()
     {
+        if(IsGrounded() && Input.GetKeyDown(KeyCode.Space))
+        {
+            _rb.AddForce(Vector3.up * jumpVelocity, ForceMode.Impulse);
+        }
         Vector3 rotation = Vector3.up * hInput;
         Quaternion angleRot = Quaternion.Euler(rotation * Time.fixedDeltaTime);
         _rb.MovePosition(this.transform.position + this.transform.forward * vInput * Time.fixedDeltaTime);
         _rb.MoveRotation(_rb.rotation * angleRot);
+    }
+    private bool IsGrounded()
+    {
+        Vector3 capsuleBottom = new Vector3(_col.bounds.center.x, _col.bounds.min.y, _col.bounds.center.z);
+        bool grounded = Physics.CheckCapsule(_col.bounds.center, capsuleBottom, distanceToGround, groundLayer, QueryTriggerInteraction.Ignore);
+        return grounded;
     }
 }
